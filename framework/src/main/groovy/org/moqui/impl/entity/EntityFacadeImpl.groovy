3b9802538767a7037d8f63ebfa39965369f29220
David E Jones <dej@dejc.com>
2016-02-12 16:54:00 -0800

    Change to use same eci in tx worker threads, now that we have a cleaner approach for that

--- framework/src/main/groovy/org/moqui/impl/entity/EntityFacadeImpl.groovy
+++ framework/src/main/groovy/org/moqui/impl/entity/EntityFacadeImpl.groovy
@@ -1462,7 +1462,7 @@ class EntityFacadeImpl implements EntityFacade {
                     this.entitySequenceBankCache.put(bankCacheKey, bank)
                 }
 
-                ecfi.getTransactionFacade().runRequireNew(null, "Error getting primary sequenced ID", true, false, {
+                ecfi.getTransactionFacade().runRequireNew(null, "Error getting primary sequenced ID", true, true, {
                     EntityValue svi = makeFind("moqui.entity.SequenceValueItem").condition("seqName", seqName)
                             .useCache(false).forUpdate(true).one()
                     if (svi == null) {
