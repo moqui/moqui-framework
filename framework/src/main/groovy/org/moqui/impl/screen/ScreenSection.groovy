afca6ac38ac21b7c7e09979d834ce3732cd28a0a
David E Jones <dej@dejc.com>
2016-03-07 15:36:54 -0800

    Screen root section now wraps widgets element in a screen element for rendering so output can be rendered at the screen level

--- framework/src/main/groovy/org/moqui/impl/screen/ScreenSection.groovy
+++ framework/src/main/groovy/org/moqui/impl/screen/ScreenSection.groovy
@@ -55,7 +55,15 @@ class ScreenSection {
             // if (location.contains("FOO")) logger.warn("====== Actions for ${location}: ${actions.writeGroovyWithLines()}")
         }
         // prep widgets
-        if (sectionNode.hasChild("widgets")) widgets = new ScreenWidgets(sectionNode.first("widgets"), location + ".widgets")
+        if (sectionNode.hasChild("widgets")) {
+            if (sectionNode.getName() == "screen") {
+                MNode widgetsNode = sectionNode.first("widgets")
+                MNode screenNode = new MNode("screen", null, null, [widgetsNode], null)
+                widgets = new ScreenWidgets(screenNode, location + ".widgets")
+            } else {
+                widgets = new ScreenWidgets(sectionNode.first("widgets"), location + ".widgets")
+            }
+        }
         // prep fail-widgets
         if (sectionNode.hasChild("fail-widgets"))
             failWidgets = new ScreenWidgets(sectionNode.first("fail-widgets"), location + ".fail-widgets")
