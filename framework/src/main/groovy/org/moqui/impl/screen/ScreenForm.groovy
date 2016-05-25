60980c962be8e4fb89ae7aedfd79fc1f1d1e3fd6
David E Jones <dej@dejc.com>
2016-05-24 17:02:56 -0700

    Cleanups, notes; changed definition of hidden form-list fields to consider conditional-field elements and only hide if hidden or ignored elements are found; added submit.@text attribute

--- framework/src/main/groovy/org/moqui/impl/screen/ScreenForm.groovy
+++ framework/src/main/groovy/org/moqui/impl/screen/ScreenForm.groovy
@@ -1074,14 +1074,17 @@ class ScreenForm {
 
 
         boolean isListFieldHidden(MNode fieldNode) {
-            MNode defaultField = fieldNode.first("default-field")
-            if (defaultField == null) return false
             String hideAttr = fieldNode.attribute("hide")
             if (hideAttr != null && hideAttr.length() > 0) {
                 boolean hideResult = ecfi.getEci().resource.condition(hideAttr, "")
                 if (hideResult) return true
             }
-            return defaultField.hasChild("hidden") || defaultField.hasChild("ignored")
+            // if default-field or any conditional-field don't have hidden or ignored elements then it's not hidden
+            MNode defaultField = fieldNode.first("default-field")
+            if (defaultField != null && !defaultField.hasChild("hidden") && !defaultField.hasChild("ignored")) return false
+            List<MNode> condFieldList = fieldNode.children("conditional-field")
+            for (MNode condField in condFieldList) if (!condField.hasChild("hidden") && !condField.hasChild("ignored")) return false
+            return true
         }
         ArrayList<FtlNodeWrapper> getListHiddenFieldList() {
             if (hiddenFieldList != null) return hiddenFieldList
