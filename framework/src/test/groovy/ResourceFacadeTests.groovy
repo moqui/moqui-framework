d029fa0b7f02e6216d4a0130dbba86c64f073a69
David E Jones <dej@dejc.com>
2016-06-18 02:04:34 -0700

    Changed jta.properties to bitronix-default-config.properties as jta.properties has been moved to moqui-atomikos

--- framework/src/test/groovy/ResourceFacadeTests.groovy
+++ framework/src/test/groovy/ResourceFacadeTests.groovy
@@ -50,7 +50,7 @@ class ResourceFacadeTests extends Specification {
         "component://tools/screen/Tools.xml" | "file" | null | "Tools.xml" | "text/xml" | true | true | false
         "component://tools/screen/ToolsFoo.xml" | "file" | null | "ToolsFoo.xml" | "text/xml" | false | false | false
         "classpath://entity/BasicEntities.xml" | "file" | null | "BasicEntities.xml" | "text/xml" | true | true | false
-        "classpath://jta.properties" | "file" | null | "jta.properties" | "text/x-java-properties" | true | true | false
+        "classpath://bitronix-default-config.properties" | "file" | null | "bitronix-default-config.properties" | "text/x-java-properties" | true | true | false
         "classpath://shiro.ini" | "file" | null | "shiro.ini" | "text/plain" | true | true | false
         "template/screen-macro/ScreenHtmlMacros.ftl" | "file" | null | "ScreenHtmlMacros.ftl" | "text/x-freemarker" | true | true | false
         "template/screen-macro" | "file" | null | "screen-macro" | "application/octet-stream" | true | false | true
