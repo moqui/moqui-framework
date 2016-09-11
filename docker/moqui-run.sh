76f8434977f5b6d62ee33352e4105135e6b6a219
David E Jones <dej@dejc.com>
2016-09-10 19:30:03 -0700

    A few improvements to docker scripts, support moqui home argument and such

--- docker/moqui-run.sh
+++ docker/moqui-run.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 
-# TODO maybe accept a parameter for the location of the moqui directory (where to find stock runtime), which runtime directory to use, etc...
-# overall make more useful for running from an arbitrary location... of course can always be edited for specific use (and generally should be)
+MOQUI_HOME="${1:-../..}"
 
 if [ ! -e runtime ]; then mkdir runtime; fi
-if [ ! -e runtime/conf ]; then cp -R ../runtime/conf runtime/; fi
-if [ ! -e runtime/lib ]; then cp -R ../runtime/lib runtime/; fi
-if [ ! -e runtime/classes ]; then cp -R ../runtime/classes runtime/; fi
-if [ ! -e runtime/log ]; then cp -R ../runtime/log runtime/; fi
-if [ ! -e runtime/txlog ]; then cp -R ../runtime/txlog runtime/; fi
-if [ ! -e runtime/db ]; then cp -R ../runtime/db runtime/; fi
-if [ ! -e runtime/elasticsearch ]; then cp -R ../runtime/elasticsearch runtime/; fi
+if [ ! -e runtime/conf ]; then cp -R $MOQUI_HOME/runtime/conf runtime/; fi
+if [ ! -e runtime/lib ]; then cp -R $MOQUI_HOME/runtime/lib runtime/; fi
+if [ ! -e runtime/classes ]; then cp -R $MOQUI_HOME/runtime/classes runtime/; fi
+if [ ! -e runtime/log ]; then cp -R $MOQUI_HOME/runtime/log runtime/; fi
+if [ ! -e runtime/txlog ]; then cp -R $MOQUI_HOME/runtime/txlog runtime/; fi
+if [ ! -e runtime/db ]; then cp -R $MOQUI_HOME/runtime/db runtime/; fi
+if [ ! -e runtime/elasticsearch ]; then cp -R $MOQUI_HOME/runtime/elasticsearch runtime/; fi
 
 docker run --rm -p 8080:8080 -v $PWD/runtime/conf:/opt/moqui/runtime/conf -v $PWD/runtime/lib:/opt/moqui/runtime/lib \
     -v $PWD/runtime/classes:/opt/moqui/runtime/classes -v $PWD/runtime/log:/opt/moqui/runtime/log \
--- docker/simple/docker-build.sh
+++ docker/simple/docker-build.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
 
-if [ -f ../../moqui-plus-runtime.war ]; then
-    echo "Building docker image from moqui-plus-runtime.war"
-    unzip -q ../../moqui-plus-runtime.war
-  elif [ -f ../../moqui.war ]; then
-    echo "Building docker image from moqui.war and runtime directory"
-    echo "NOTE: this includes everything in the runtime directory, it is better to run 'gradle addRuntime' first and use the moqui-plus-runtime.war file for the docker image"
-    unzip -q ../../moqui.war
-    cp -R ../../runtime .
-  else
-    echo "Could not find moqui-plus-runtime.war or moqui.war"
+MOQUI_HOME="${1:-../..}"
+
+if [ -f $MOQUI_HOME/moqui-plus-runtime.war ]
+then
+  echo "Building docker image from moqui-plus-runtime.war"
+  echo
+  unzip -q $MOQUI_HOME/moqui-plus-runtime.war
+elif [ -f $MOQUI_HOME/moqui.war ]
+then
+  echo "Building docker image from moqui.war and runtime directory"
+  echo "NOTE: this includes everything in the runtime directory, it is better to run 'gradle addRuntime' first and use the moqui-plus-runtime.war file for the docker image"
+  echo
+  unzip -q $MOQUI_HOME/moqui.war
+  cp -R $MOQUI_HOME/runtime .
+else
+    echo "Could not find $MOQUI_HOME/moqui-plus-runtime.war or $MOQUI_HOME/moqui.war"
+    echo "Build moqui first, for example 'gradle build addRuntime' or 'gradle load addRuntime'"
+    echo
     exit 1
 fi
 
