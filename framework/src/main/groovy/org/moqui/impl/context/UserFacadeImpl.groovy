e595095435f26e695c5c76d1808f5103caf616a4
Solomon Bessire <sib@sssonline.com>
2016-11-02 15:24:12 -0700

    Fix typo on 7 day selection.  Calculation adjusted as well.

--- framework/src/main/groovy/org/moqui/impl/context/UserFacadeImpl.groovy
+++ framework/src/main/groovy/org/moqui/impl/context/UserFacadeImpl.groovy
@@ -387,18 +387,18 @@ class UserFacadeImpl implements UserFacade {
             thruCal = (Calendar) fromCal.clone()
             thruCal.add(Calendar.WEEK_OF_YEAR, 1)
         } else if (period == "7d") {
-            fromCal.add(Calendar.DAY_OF_YEAR, (offset+1) * 7)
+            fromCal.add(Calendar.DAY_OF_YEAR, offset * 7)
             thruCal = (Calendar) fromCal.clone()
-            thruCal.add(Calendar.DAY_OF_YEAR, 30)
+            thruCal.add(Calendar.DAY_OF_YEAR, 8)
         } else if (period == "month") {
             fromCal.set(Calendar.DAY_OF_MONTH, fromCal.getActualMinimum(Calendar.DAY_OF_MONTH))
             fromCal.add(Calendar.MONTH, offset)
             thruCal = (Calendar) fromCal.clone()
             thruCal.add(Calendar.MONTH, 1)
         } else if (period == "30d") {
-            fromCal.add(Calendar.DAY_OF_YEAR, (offset+1) * 30)
+            fromCal.add(Calendar.DAY_OF_YEAR, offset * 30)
             thruCal = (Calendar) fromCal.clone()
-            thruCal.add(Calendar.DAY_OF_YEAR, 30)
+            thruCal.add(Calendar.DAY_OF_YEAR, 31)
         } else if (period == "year") {
             fromCal.set(Calendar.DAY_OF_YEAR, fromCal.getActualMinimum(Calendar.DAY_OF_YEAR))
             fromCal.add(Calendar.YEAR, offset)
