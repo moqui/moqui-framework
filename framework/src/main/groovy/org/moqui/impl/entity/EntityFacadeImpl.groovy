0814abfcdc8e05414209b88d971073b8394de88a
David E Jones <dej@dejc.com>
2017-04-24 23:24:15 -0700

    Added easy method to get db conf name for an entity

--- framework/src/main/groovy/org/moqui/impl/entity/EntityFacadeImpl.groovy
+++ framework/src/main/groovy/org/moqui/impl/entity/EntityFacadeImpl.groovy
@@ -1122,6 +1122,11 @@ class EntityFacadeImpl implements EntityFacade {
         return ecfi.confXmlRoot.first("database-list")
                 .first({ MNode it -> it.name == 'database' && it.attribute("name") == confName })
     }
+    String getDatabaseConfName(String entityName) {
+        MNode dsNode = getDatasourceNode(getEntityGroupName(entityName))
+        if (dsNode == null) return null
+        return dsNode.attribute("database-conf-name")
+    }
 
     MNode getDatasourceNode(String groupName) {
         MNode node = datasourceNodeByGroupName.get(groupName)
