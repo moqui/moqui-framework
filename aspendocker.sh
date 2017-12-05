0955b563bb1227bd4f5eb899895680a4cb61bb68
Solomon Bessire <sib@sssonline.com>
2017-12-05 12:13:56 -0800

    Did not intend to commit shell scripts.

--- aspendocker.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-gradle cleanAll gitPullAll
-gradle addRuntime
-cd docker/simple
-./docker-build.sh ../.. sssonline/aspen
-docker tag sssonline/aspen sssonline/aspen:`date +%Y-%m-%d`
-docker push sssonline/aspen
-ssh moqui@207.183.240.72
--- gebbersenv.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-gradle cleanAll load -Ptypes=seed,seed-initial,install
-java -jar moqui.war load types=demo components=webroot,gebbers
-gradle runtime:component:gebbers:test
-java -jar moqui.war
