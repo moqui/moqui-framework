4111d1cd300dbbb67cb74f50fef634bad0e134c7
David E Jones <dej@dejc.com>
2018-08-20 17:37:13 -0700

    Add example docker based cluster configuration for cluster testing with external elasticsearch server, internal running with no data as coordination node, and changes to support hazelcast cluster in docker image

--- docker/clean.sh
+++ docker/clean.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
 rm -Rf runtime/
+rm -Rf runtime1/
+rm -Rf runtime2/
 rm -Rf db/
+rm -Rf elasticsearch/data
+rm -Rf elasticsearch/logs
 
 docker rm moqui-server
 docker rm moqui-database
