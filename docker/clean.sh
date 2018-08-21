d2d52cce1110427cd86f9a544dcde0ce95cf841a
David E Jones <dej@dejc.com>
2018-08-21 16:35:02 -0700

    Update moqui-cluster1-compose.yml for elasticsearch 6.3.2, -oss version so really annoying x-pack crap isn't there; some improvements in the way elasticsearch is configured

--- docker/clean.sh
+++ docker/clean.sh
@@ -4,8 +4,8 @@ rm -Rf runtime/
 rm -Rf runtime1/
 rm -Rf runtime2/
 rm -Rf db/
-rm -Rf elasticsearch/data
-rm -Rf elasticsearch/logs
+rm -Rf elasticsearch/data/nodes
+rm elasticsearch/logs/*.log
 
 docker rm moqui-server
 docker rm moqui-database
