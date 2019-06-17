moqui/moqui-framework

commit 600dda0ef3a0c5dabc9bfcce3e528c3da64552c3

In entity find with MNode support groovy string expand in select-field.@field-name attribute (used commonly with entity-find under form-list); for search form inputs pagination in form-list or XML Actions only do count query if results are not less than the page size, small optimization

framework/src/main/groovy/org/moqui/impl/screen/ScreenForm.groovy
@@ -1712,9 +1712,11 @@ class ScreenForm {
                         pageSize = efList.pageSize
                         pageIndex = efList.pageIndex
                     } else {
-                        count = ef.count()
                         pageIndex = ef.pageIndex
                         pageSize = ef.pageSize
+                        // this can be expensive, only get count if efList size is equal to pageSize (can skip if no paginate needed)
+                        if (efList.size() < pageSize) count = efList.size()
+                        else count = ef.count()
                     }
                     long maxIndex = (new BigDecimal(count-1)).divide(new BigDecimal(pageSize), 0, BigDecimal.ROUND_DOWN).longValue()
                     long pageRangeLow = (pageIndex * pageSize) + 1

framework/template/XmlActions.groovy.ftl
@@ -174,9 +174,10 @@ ${.node}
                 ${listName}PageIndex = ${listName}.pageIndex
                 ${listName}PageSize = ${listName}Count > 20 ? ${listName}Count : 20
             } else {
-                ${listName}Count = ${listName}_xafind.count()
                 ${listName}PageIndex = ${listName}_xafind.pageIndex
                 ${listName}PageSize = ${listName}_xafind.pageSize
+                if (${listName}.size() < ${listName}PageSize) { ${listName}Count = ${listName}.size() }
+                else { ${listName}Count = ${listName}_xafind.count() }
             }
         </#if>
         ${listName}PageMaxIndex = ((BigDecimal) (${listName}Count - 1)).divide(${listName}PageSize ?: (${listName}Count - 1), 0, BigDecimal.ROUND_DOWN) as int

framework/src/main/groovy/org/moqui/impl/entity/EntityFacadeImpl.groovy
@@ -1356,7 +1356,12 @@ class EntityFacadeImpl implements EntityFacade {
         if (offset != null && !offset.isEmpty()) ef.offset(Integer.valueOf(offset))
         String limit = node.attribute("limit")
         if (limit != null && !limit.isEmpty()) ef.limit(Integer.valueOf(limit))
-        for (MNode sf in node.children("select-field")) ef.selectField(sf.attribute("field-name"))
+        for (MNode sf in node.children("select-field")) {
+            String fieldToSelect = sf.attribute("field-name")
+            if (fieldToSelect == null || fieldToSelect.isEmpty()) continue
+            if (fieldToSelect.contains('${')) fieldToSelect = ecfi.resourceFacade.expandNoL10n(fieldToSelect, null)
+            ef.selectField(fieldToSelect)
+        }
         for (MNode ob in node.children("order-by")) ef.orderBy(ob.attribute("field-name"))
 
         if (node.hasChild("search-form-inputs")) {

commit 683ddd6b0cdc4f75a2f6fc156fdcf3996516370f

Fix issue with pagination counts on last page of multi page results, caused by optimization change in commit 600dda0e on 19 Dec 2018

framework/src/main/groovy/org/moqui/impl/screen/ScreenForm.groovy
@@ -1724,23 +1724,24 @@ class ScreenForm {
                     if (ef.getLimit() == null) {
                         count = efList.size()
                         pageSize = count > 20 ? count : 20
-                        pageIndex = efList.pageIndex
+                        pageIndex = efList.getPageIndex()
                     } else if (useCache) {
                         count = efList.size()
                         efList.filterByLimit(sfiNode.attribute("input-fields-map"), true)
-                        pageSize = efList.pageSize
-                        pageIndex = efList.pageIndex
+                        pageSize = efList.getPageSize()
+                        pageIndex = efList.getPageIndex()
                     } else {
                         pageIndex = ef.pageIndex
                         pageSize = ef.pageSize
                         // this can be expensive, only get count if efList size is equal to pageSize (can skip if no paginate needed)
-                        if (efList.size() < pageSize) count = efList.size()
+                        if (efList.size() < pageSize) count = efList.size() + pageSize * pageIndex
                         else count = ef.count()
                     }
                     long maxIndex = (new BigDecimal(count-1)).divide(new BigDecimal(pageSize), 0, BigDecimal.ROUND_DOWN).longValue()
                     long pageRangeLow = (pageIndex * pageSize) + 1
                     long pageRangeHigh = (pageIndex * pageSize) + pageSize
                     if (pageRangeHigh > count) pageRangeHigh = count
+                    // logger.info("count ${count} pageSize ${pageSize} maxIndex ${maxIndex} pageRangeLow ${pageRangeLow} pageRangeHigh ${pageRangeHigh}")
 
                     context.put(listName.concat("Count"), count)
                     context.put(listName.concat("PageIndex"), pageIndex)

framework/template/XmlActions.groovy.ftl
@@ -172,12 +172,12 @@ ${.node}
         <#if !useCache>
             if (${listName}_xafind.getLimit() == null) {
                 ${listName}Count = ${listName}.size()
-                ${listName}PageIndex = ${listName}.pageIndex
+                ${listName}PageIndex = ${listName}.getPageIndex()
                 ${listName}PageSize = ${listName}Count > 20 ? ${listName}Count : 20
             } else {
-                ${listName}PageIndex = ${listName}_xafind.pageIndex
-                ${listName}PageSize = ${listName}_xafind.pageSize
-                if (${listName}.size() < ${listName}PageSize) { ${listName}Count = ${listName}.size() }
+                ${listName}PageIndex = ${listName}_xafind.getPageIndex()
+                ${listName}PageSize = ${listName}_xafind.getPageSize()
+                if (${listName}.size() < ${listName}PageSize) { ${listName}Count = ${listName}.size() + ${listName}PageIndex * ${listName}PageSize }
                 else { ${listName}Count = ${listName}_xafind.count() }
             }
         </#if>