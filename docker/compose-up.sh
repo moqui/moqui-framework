79bfd38de14f890b4052ad5683aacae6711932b7
Michal Rovňaník <mrovnanik@users.noreply.github.com>
2019-06-18 22:13:17 +0200

    Customization of docker cnofig. (#32)

--- docker/compose-up.sh
+++ docker/compose-up.sh
@@ -15,7 +15,7 @@ if [ ! -e runtime/classes ]; then cp -R $MOQUI_HOME/runtime/classes runtime/; fi
 if [ ! -e runtime/log ]; then cp -R $MOQUI_HOME/runtime/log runtime/; fi
 if [ ! -e runtime/txlog ]; then cp -R $MOQUI_HOME/runtime/txlog runtime/; fi
 if [ ! -e runtime/db ]; then cp -R $MOQUI_HOME/runtime/db runtime/; fi
-if [ ! -e runtime/elasticsearch ]; then cp -R $MOQUI_HOME/runtime/elasticsearch runtime/; fi
+# if [ ! -e runtime/elasticsearch ]; then cp -R $MOQUI_HOME/runtime/elasticsearch runtime/; fi
 
 # set the project name to 'moqui', network will be called 'moqui_default'
 docker-compose -f $COMP_FILE -p moqui up -d
