8a272e5dad72838e9d3e4c521b7fffa9842a7023
David E Jones <dej@dejc.com>
2019-11-13 19:21:15 -0800

    Add ExecutionContext.getElastic() method

--- framework/src/main/java/org/moqui/context/ExecutionContext.java
+++ framework/src/main/java/org/moqui/context/ExecutionContext.java
@@ -83,6 +83,9 @@ public interface ExecutionContext {
     /** For interactions with a relational database. */
     @Nonnull EntityFacade getEntity();
 
+    /** For interactions with ElasticSearch using the built in HTTP REST client. */
+    @Nonnull ElasticFacade getElastic();
+
     /** For calling services (local or remote, sync or async or scheduled). */
     @Nonnull ServiceFacade getService();
 
