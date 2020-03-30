6bea3fba47400377661e324afa61ed5bc03d3318
Michal Rovnanik on WS <michal@rovnanik.sk>
2020-03-30 20:30:46 +0200

    More docker build options by introducing new Dockerfile.

--- docker/create.app.image.azure.bat
+++ docker/create.app.image.azure.bat
@@ -1,12 +1,14 @@
 SET image_name=%~1
+SET docker_repo=%~2
+SET dockerfile=%~3
 
 REM docker stop moqui-app
 REM docker rm moqui-app
 REM #docker rmi $(docker images |grep 'moqui'
 
 REM "Build moqui-plus-runtime.war"
-cd ..
+pushd ..
 call gradlew addRuntime -x test
 
-cd docker\simple
-docker-build.bat ../.. %image_name%
+pushd docker\simple
+docker-build.bat ../.. %image_name% %docker_repo% %dockerfile%
--- docker/simple/docker-build.bat
+++ docker/simple/docker-build.bat
@@ -2,13 +2,17 @@ ECHO OFF
 REM "Usage: docker-build.sh [<moqui directory like ../..>] [<group/name:tag>]"
 SET MOQUI_HOME=%~1
 SET NAME_TAG=%~2
+SET REPO_NAME=%~3
+SET DOCKERFILE=%~3
+
+IF "%DOCKERFILE%"=="" (SET DOCKERFILE=Dockerfile)
 
 REM "Building docker image from moqui-plus-runtime.war"
 7z x %MOQUI_HOME%\moqui-plus-runtime.war
 
 REM docker build - < Dockerfile -t %NAME_TAG%
-REM cd docker\simple
-docker build -t %NAME_TAG% .
+docker build -f %DOCKERFILE% -t %NAME_TAG% .
+IF NOT "%REPO_NAME%"=="" (docker push %REPO_NAME%/%NAME_TAG%)
 
 REM delete all remains
 rmdir /Q /S META-INF 
@@ -17,11 +21,3 @@ rmdir /Q /S execlib
 rmdir /Q /S runtime
 del *.class
 del Procfile
-
-REM delete runtime
-cd ..
-rmdir /Q /S runtime
-
-REM prepare database
-REM cd ..
-REM gradlew build -x test
