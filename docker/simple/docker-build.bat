9f41be0e40e9add58ba20e3203048aedecd243e8
Michal Rovnanik on WS <michal@rovnanik.sk>
2020-03-30 20:37:14 +0200

    Reverting few changes - multiple Dockerfiles

--- docker/simple/docker-build.bat
+++ docker/simple/docker-build.bat
@@ -3,15 +3,12 @@ REM "Usage: docker-build.sh [<moqui directory like ../..>] [<group/name:tag>]"
 SET MOQUI_HOME=%~1
 SET NAME_TAG=%~2
 SET REPO_NAME=%~3
-SET DOCKERFILE=%~3
-
-IF "%DOCKERFILE%"=="" (SET DOCKERFILE=Dockerfile)
 
 REM "Building docker image from moqui-plus-runtime.war"
 7z x %MOQUI_HOME%\moqui-plus-runtime.war
 
 REM docker build - < Dockerfile -t %NAME_TAG%
-docker build -f %DOCKERFILE% -t %NAME_TAG% .
+docker build -t %NAME_TAG% .
 IF NOT "%REPO_NAME%"=="" (docker push %REPO_NAME%/%NAME_TAG%)
 
 REM delete all remains
