eae54a23dbe71610411cae312d9e82c3136a139a
David E Jones <dej@dejc.com>
2020-09-26 17:16:57 -0700

    Add webapp_upload_executable_allow property, defaults to false, and used for webapp.@upload-executable-allow attribute plus supporting code in WebFacadeImpl to check uploads for executable file byte markers

---
+++ framework/src/main/java/org/moqui/context/WebMediaTypeException.java
@@ -0,0 +1,23 @@
+/*
+ * This software is in the public domain under CC0 1.0 Universal plus a
+ * Grant of Patent License.
+ * 
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to the
+ * public domain worldwide. This software is distributed without any
+ * warranty.
+ * 
+ * You should have received a copy of the CC0 Public Domain Dedication
+ * along with this software (see the LICENSE.md file). If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org.moqui.context;
+
+import org.moqui.BaseArtifactException;
+
+import java.util.Deque;
+
+public class WebMediaTypeException extends BaseArtifactException {
+    public WebMediaTypeException(String str) { super(str); }
+    public WebMediaTypeException(String str, Throwable nested) { super(str, nested); }
+}
