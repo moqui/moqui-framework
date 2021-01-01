34906992660bb2c4643a4203c328f6ba122ae395
Michal Rovnanik on WS <michal@rovnanik.sk>
2021-01-01 18:21:21 +0100

    Fix docker build scripts, supports versioning

--- docker/create.app.image.azure.bat
+++ docker/create.app.image.azure.bat
@@ -1,13 +1,17 @@
 SET image_name=%~1
 SET docker_repo=%~2
+SET moqui_conf_file=%~3
+SET image_version=%~4
 
 REM docker stop moqui-app
 REM docker rm moqui-app
 REM #docker rmi $(docker images |grep 'moqui'
 
-REM "Build moqui-plus-runtime.war"
+ECHO    Build moqui-plus-runtime.war with 'conf=%moqui_conf%'
 pushd ..
-call gradlew addRuntime -x test
+call gradlew addRuntime -Dmoqui.conf=%moqui_conf% addRuntime -x test
 
+
+ECHO    Building Docker image '%image_name%' into '%docker_repo%'
 pushd docker\simple
-docker-build.bat ../.. %image_name% %docker_repo%
+docker-build.bat ../.. %image_name% %docker_repo% %image_version%
--- docker/simple/docker-build.bat
+++ docker/simple/docker-build.bat
@@ -3,13 +3,16 @@ REM "Usage: docker-build.sh [<moqui directory like ../..>] [<group/name:tag>]"
 SET MOQUI_HOME=%~1
 SET NAME_TAG=%~2
 SET REPO_NAME=%~3
+SET VERSION=%~4
+
+IF "%VERSION%"=="" (SET VERSION=latest)
 
 REM "Building docker image from moqui-plus-runtime.war"
 7z x %MOQUI_HOME%\moqui-plus-runtime.war
 
 REM docker build - < Dockerfile -t %NAME_TAG%
-docker build -t %NAME_TAG% .
-IF NOT "%REPO_NAME%"=="" (docker tag %NAME_TAG% %REPO_NAME%/%NAME_TAG% && docker push %REPO_NAME%/%NAME_TAG%)
+docker build -t %NAME_TAG%:%VERSION%  .
+IF NOT "%REPO_NAME%"=="" (docker tag %NAME_TAG%:%VERSION% %REPO_NAME%/%NAME_TAG%:%VERSION% && docker push %REPO_NAME%/%NAME_TAG%:%VERSION%)
 
 REM delete all remains
 rmdir /Q /S META-INF 
