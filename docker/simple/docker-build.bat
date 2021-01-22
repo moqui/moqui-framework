83a044d2aadaf3a02bb6eab33badbc6f20ef5e89
Michal Rovnanik on WS <michal@rovnanik.sk>
2021-01-22 10:22:14 +0100

    docker related changes, wait-for-postgres script added, further refactor required - the custom Dockerfile should be project based, repo-wise

--- docker/simple/docker-build.bat
+++ docker/simple/docker-build.bat
@@ -3,14 +3,16 @@ REM "Usage: docker-build.sh [<moqui directory like ../..>] [<group/name:tag>]"
 SET MOQUI_HOME=%~1
 SET NAME_TAG=%~2
 SET VERSION=%~3
+SET DOCKERFILE=%~4
 
 IF "%VERSION%"=="" SET VERSION=latest
 
 REM "Building docker image from moqui-plus-runtime.war"
 7z x %MOQUI_HOME%\moqui-plus-runtime.war
+xcopy "..\wait-for-postgres.sh" . /Y
 
 REM docker build - < Dockerfile -t %NAME_TAG%
-docker build -t %NAME_TAG%:%VERSION% .
+IF NOT DEFINED DOCKERFILE ( ECHO Building the standard way && docker build -t %NAME_TAG%:%VERSION% . ) ELSE ( ECHO Building using specified docker file '%DOCKERFILE%' && docker build -t %NAME_TAG%:%VERSION% -f %DOCKERFILE% . )
 
 REM delete all remains
 rmdir /Q /S META-INF 
@@ -19,3 +21,4 @@ rmdir /Q /S execlib
 rmdir /Q /S runtime
 del *.class
 del Procfile
+del wait-for-postgres.sh
