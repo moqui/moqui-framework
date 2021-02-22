c8a69fa2b25cb19603d5094f27226419c5ca53ff
Michal Rovnanik on WS <michal@rovnanik.sk>
2021-02-22 16:39:15 +0100

    new gradle task allows for custom port startup, common handling of paginated query construction

--- framework/src/main/groovy/org/moqui/impl/ViUtilities.groovy
+++ framework/src/main/groovy/org/moqui/impl/ViUtilities.groovy
@@ -13,26 +13,13 @@
  */
 package org.moqui.impl
 
-import groovy.transform.CompileStatic
-import groovy.transform.TypeChecked
-import groovy.transform.TypeCheckingMode
-
-import java.nio.charset.Charset
-import java.sql.Time
-import java.sql.Timestamp
-import java.time.temporal.ChronoUnit
-import java.time.temporal.TemporalUnit
-import java.util.regex.Pattern
-
-import org.w3c.dom.Element
-
+import groovy.json.JsonSlurper
+import java.sql.Connection
 import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 /** These are utilities that should exist elsewhere, but I can't find a good simple library for them, and they are
  * stupid but necessary for certain things.
  */
-@CompileStatic
 class ViUtilities {
     static final String removeNonumericCharacters(String inputValue) {
         return inputValue.replaceAll("[^\\d]", "")
@@ -41,4 +28,191 @@ class ViUtilities {
     static final String removeCommas(String inputValue) {
         return inputValue.replaceAll(",", ".")
     }
+
+    static String calcPagedQuery(String query, Integer pageIndex, Integer pageSize, Integer limit)
+    {
+        String resQuery = query
+        pageIndex = Math.max(0, pageIndex - 1)
+
+        // limit
+        if (limit > 0 && pageSize == 0)
+        {
+            resQuery = "${resQuery} limit ${limit}"
+        } else if (pageSize > 0)
+        {
+            resQuery = "${resQuery} limit ${pageSize}"
+        }
+
+        // offset
+        if (pageIndex > 0)
+        {
+            resQuery = "${resQuery} offset ${pageIndex * pageSize}"
+        }
+
+        return resQuery
+    }
+
+    static String calcTotalQuery(String query)
+    {
+        // first, calculate query
+        String[] splitQuery = query.split(" from ")
+        if (splitQuery.length != 2) return ""
+
+        // we need to call `select count(*) ... ` to get the total count
+        return "select count(*) as total from ${splitQuery[1]}"
+    }
+
+    static HashMap<String, Integer> paginationInfo(clLoadTotal, String query, Integer pageIndex, Integer pageSize)
+    {
+        HashMap<String, Integer> result = new HashMap<>()
+
+        // fix index
+        pageIndex = Math.max(0, pageIndex - 1)
+
+        // calculate total count query
+        String totalCountQuery = calcTotalQuery(query)
+        if (!totalCountQuery) return result
+
+        // load total using closure
+        Integer viIdListCount = clLoadTotal(totalCountQuery)
+
+        // when loading total returns -1, quit
+        if (-1 == viIdListCount) return result
+
+        // when total count is less than page size, no pagination
+        if (pageSize > viIdListCount) return result
+
+        Integer viIdListPageIndex = pageIndex
+        Integer viIdListPageSize = pageSize
+        Integer viIdListPageMaxIndex = ((BigDecimal) (viIdListCount - 1)).divide(viIdListPageSize as BigDecimal, 0, BigDecimal.ROUND_DOWN) as int
+        Integer viIdListPageRangeLow = viIdListPageIndex * viIdListPageSize + 1
+        Integer viIdListPageRangeHigh = (viIdListPageIndex * viIdListPageSize) + viIdListPageSize
+        if (viIdListPageRangeHigh > viIdListCount) viIdListPageRangeHigh = viIdListCount
+
+        //calculate 'to' item
+        def maxTo = Math.min(viIdListPageIndex * viIdListPageSize + viIdListPageSize, viIdListCount)
+
+        result.put('total', viIdListCount)
+        result.put('per_page', viIdListPageSize)
+        result.put('current_page', viIdListPageIndex + 1)
+        result.put('last_page', viIdListPageMaxIndex + 1)
+        //paginationData.put('next_page_url', searchUrl + '?pageIndex=2')
+        //paginationData.put('prev_page_url', null)
+        result.put('from', viIdListPageIndex * viIdListPageSize + 1)
+        result.put('to', maxTo)
+
+        return result
+    }
+
+    static HashMap<String, Object> executeQuery(
+            Connection conn,
+            Logger logger,
+            String query,
+            Integer pageIndex = 1,
+            Integer pageSize = 20,
+            Integer limit = 500)
+    {
+        String queryMod = calcPagedQuery(query, pageIndex, pageSize, limit)
+        JsonSlurper slurper = new JsonSlurper()
+        def rs = null
+        def stmt = null
+        def columns = new ArrayList<String>()
+        def records = new ArrayList<ArrayList>()
+
+        if (queryMod.toUpperCase().startsWith("SELECT")) {
+            stmt = conn.createStatement()
+            rs = stmt.executeQuery(queryMod)
+            if (rs != null) {
+                def rsmd = rs.getMetaData()
+                def columnCount = rsmd.getColumnCount()
+                for (int i = 1; i <= columnCount; i++) columns.add(rsmd.getColumnName(i))
+
+                def limitReached = false
+                while (rs.next()) {
+                    if (limit > 0 && records.size() >= limit) {
+                        limitReached = true
+                        break
+                    }
+                    def record = new ArrayList<Object>(columnCount)
+                    for (int i = 1; i <= columnCount; i++)
+                    {
+                        def obj = rs.getObject(i)
+                        String tp = "unknown"
+                        if (obj.hasProperty("type")) tp = obj["type"]
+                        switch(tp)
+                        {
+                            case 'jsonb':
+                                Object objVal = slurper.parseText(obj.value)
+                                record.add(objVal)
+                                break
+                            default:
+                                record.add(obj)
+                        }
+                    }
+                    records.add(record)
+                }
+                rs.close()
+
+                if (limitReached) {
+                    logger.debug("Only showing first ${limit} rows")
+                } else {
+                    if (records)
+                    {
+                        logger.debug("Showing all ${records.size()}")
+                    } else {
+                        logger.debug("No records returned")
+                    }
+                }
+            }
+        } else {
+            stmt = conn.createStatement()
+            def rowsAffected = stmt.executeUpdate(queryMod as String)
+            logger.debug("Query altered ${rowsAffected} rows")
+        }
+
+        // close statement
+        if (stmt != null) { try { stmt.close() } catch (Exception e) { /* Ignore */ } }
+
+        // manipulate result into a map
+        HashMap<String, Object> result = new HashMap<>()
+
+        if (!records) return result
+
+        def resultset = []
+        for (r in records)
+        {
+            def rec = [:]
+            columns.eachWithIndex{ String col, int i ->
+                rec.put(col, r[i]?r[i]:'')
+            }
+            resultset.push(rec)
+        }
+
+        result.put('data', resultset)
+        result.put('pagination', paginationInfo({totalQuery ->
+            // default - nothing found
+            Integer total = -1
+
+            def clStmt = conn.createStatement()
+            def clRs = clStmt.executeQuery(totalQuery)
+            // if nothing, close and return default
+            if (!clRs) return total
+
+            while (clRs.next())
+            {
+                total = clRs.getObject(1).toString().toInteger()
+            }
+
+            // close all db related objects
+            if (clRs != null) clRs.close()
+            if (clStmt != null) clStmt.close()
+
+            return total
+        }, query, pageIndex, pageSize))
+
+        // check pagination and remove if none provided
+        if (result.pagination == [:]) result.remove('pagination')
+
+        return result
+    };
 }
