cc6ba7163747e7a6a81bf54c659a40d9a2f25b95
David E Jones <dej@dejc.com>
2021-03-10 20:15:21 -0800

    Add EntityValue.touchField() method, change decrypt to return a magic string instead of the encrypted value on decrypt failure, and don't allow setting that magic value to avoid data loss; change rewrite#EntityEncryptedFields service to use new touchField() method and not rewrite records with any field that has a decrypt failure

--- framework/src/main/java/org/moqui/entity/EntityValue.java
+++ framework/src/main/java/org/moqui/entity/EntityValue.java
@@ -37,6 +37,8 @@ public interface EntityValue extends Map<String, Object>, Externalizable, Compar
     boolean isModified();
     /** Returns true if the field has been modified */
     boolean isFieldModified(String name);
+    /** Treat field as modified for update() even if not to force a resend to the DB */
+    EntityValue touchField(String name);
     /** Returns true if a value for the field is set, even if it is null */
     boolean isFieldSet(String name);
     /** Returns true if the name is a valid field name for the entity this is a value of,
