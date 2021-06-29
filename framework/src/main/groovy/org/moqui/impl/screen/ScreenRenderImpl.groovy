ccd6c2fd6d2bcc79014b84b31a87d186651bd62d
Michael Jones <acetousk@gmail.com>
2021-06-29 13:09:28 -0600

    Clean up project for pull request; add comments to clarify services

--- framework/src/main/groovy/org/moqui/impl/screen/ScreenRenderImpl.groovy
+++ framework/src/main/groovy/org/moqui/impl/screen/ScreenRenderImpl.groovy
@@ -425,8 +425,8 @@ class ScreenRenderImpl implements ScreenRender {
                 // require a moquiSessionToken parameter for all but get
                 if (request.getMethod().toLowerCase() != "get" && webappInfo != null && webappInfo.requireSessionToken &&
                         targetTransition.getRequireSessionToken() &&
-                        !"true".equals(request.getAttribute("moqui.session.token.created"))
-                        && !"true".equals(request.getAttribute("moqui.request.authenticated"))) {
+                        !"true".equals(request.getAttribute("moqui.session.token.created")) &&
+                        !"true".equals(request.getAttribute("moqui.request.authenticated"))) {
                     String passedToken = (String) ec.web.getParameters().get("moquiSessionToken")
                     if (!passedToken) passedToken = request.getHeader("moquiSessionToken") ?:
                             request.getHeader("SessionToken") ?: request.getHeader("X-CSRF-Token")
