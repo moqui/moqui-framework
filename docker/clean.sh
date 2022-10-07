695824b0a55f43797abdf7d249a28012f56db596
acetousk <acetousk@users.noreply.github.com>
2022-10-07 16:54:11 -0600

    Fix docker open search and Change default image because of deprecation of openjdk (#544)
    * Remove myaddons.xml from .gitignore
    
    * Add myaddons.xml and aws deploy workflow
    
    * Allow open search with docker and clean up the docker workflow a bit
    
    * Test aws action
    
    * Test AWS Action
    
    * Test AWS Build
    
    * Test AWS lint
    
    * Test AWS
    
    * Fix docker open search and keep the basically same bash logic, Change default image because of deprecation of openjdk
    
    * Fix database volume in moqui-ng-pg-compose.xml
    
    * Remove aws workflow
    
    * Remove myaddons.xml
    
    * Keep compose-up.sh the same, and move docker-build.sh and compose-up.sh to build-compose-up.sh
    
    * Flip build and compose-up

--- docker/clean.sh
+++ docker/clean.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+search_name=opensearch
+if [ -d runtime/opensearch/bin ]; then search_name=opensearch;
+elif [ -d runtime/elasticsearch/bin ]; then search_name=elasticsearch;
+fi
+
 rm -Rf runtime/
 rm -Rf runtime1/
 rm -Rf runtime2/
 rm -Rf db/
-rm -Rf elasticsearch/data/nodes
-rm elasticsearch/logs/*.log
+rm -Rf $search_name/data/nodes
+rm $search_name/logs/*.log
 
 docker rm moqui-server
 docker rm moqui-database
