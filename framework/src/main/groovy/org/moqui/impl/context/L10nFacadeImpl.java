cabf8a2149ae88cf3f14c44151d5bb0997a7d777
Wei Zhang <zhangwei1979@outlook.com>
2022-11-14 11:34:58 +0800

    Fixed a runtime error if Currency is BTC

--- framework/src/main/groovy/org/moqui/impl/context/L10nFacadeImpl.java
+++ framework/src/main/groovy/org/moqui/impl/context/L10nFacadeImpl.java
@@ -116,7 +116,15 @@ public class L10nFacadeImpl implements L10nFacade {
             }
         }
 
-        Currency currency = uomId != null && uomId.length() > 0 ? Currency.getInstance(uomId) : null;
+        Currency currency = null;
+        if(uomId != null && uomId.length() > 0) {
+            try {
+                currency = Currency.getInstance(uomId);
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
+
         if (locale == null) locale = getLocale();
         if (currency != null) {
             NumberFormat nf = NumberFormat.getCurrencyInstance(locale);
