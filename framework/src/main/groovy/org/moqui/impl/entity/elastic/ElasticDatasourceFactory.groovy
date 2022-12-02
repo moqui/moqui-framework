1b8aa5c575798a4022de32c81ba881aae4438b8f
David E Jones <dej@dejc.com>
2022-12-02 14:05:47 -0800

    Add config for 'logging' entity datasource that by default uses the 'default' ElasticFacade cluster, also add 'logging' as a valid value for the entity.@use attribute; add example extend-entity to move to logging group (commented) and some comments about migration options; initial implementation of ElasticDatasourceFactory, work still need on find, eli, and value classes

--- framework/src/main/groovy/org/moqui/impl/entity/elastic/ElasticDatasourceFactory.groovy
+++ framework/src/main/groovy/org/moqui/impl/entity/elastic/ElasticDatasourceFactory.groovy
@@ -14,9 +14,11 @@
 package org.moqui.impl.entity.elastic
 
 import groovy.transform.CompileStatic
+import org.moqui.context.ElasticFacade
 import org.moqui.entity.*
 import org.moqui.impl.entity.EntityDefinition
 import org.moqui.impl.entity.EntityFacadeImpl
+import org.moqui.impl.entity.FieldInfo
 import org.moqui.util.MNode
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -42,9 +44,9 @@ class ElasticDatasourceFactory implements EntityDatasourceFactory {
 
     protected EntityFacadeImpl efi
     protected MNode datasourceNode
-    protected String indexPrefix
+    protected String indexPrefix, clusterName
 
-    protected Set<String> checkedEntitySet = new HashSet<String>()
+    protected Set<String> checkedEntityIndexSet = new HashSet<String>()
 
     ElasticDatasourceFactory() { }
 
@@ -57,7 +59,8 @@ class ElasticDatasourceFactory implements EntityDatasourceFactory {
         // init the DataSource
         MNode inlineOtherNode = datasourceNode.first("inline-other")
         inlineOtherNode.setSystemExpandAttributes(true)
-        indexPrefix = inlineOtherNode.attribute("index-prefix")
+        indexPrefix = inlineOtherNode.attribute("index-prefix") ?: ""
+        clusterName = inlineOtherNode.attribute("cluster-name") ?: "default"
 
         return this
     }
@@ -69,30 +72,27 @@ class ElasticDatasourceFactory implements EntityDatasourceFactory {
     @Override
     boolean checkTableExists(String entityName) {
         EntityDefinition ed
-        // just ignore EntityException on getEntityDefinition
         try { ed = efi.getEntityDefinition(entityName) }
         catch (EntityException e) { return false }
-        // may happen if all entity names includes a DB view entity or other that doesn't really exist
         if (ed == null) return false
 
-        String fullEntityName = ed.getFullEntityName()
-        if (checkedEntitySet.contains(fullEntityName)) return true
+        String indexName = getIndexName(ed)
+        if (checkedEntityIndexSet.contains(indexName)) return true
 
-        // TODO
-        // if (TODO) return false
+        ElasticFacade.ElasticClient elasticClient = efi.ecfi.elasticFacade.getClient(clusterName)
+        if (!elasticClient.indexExists(indexName)) return false
 
-        checkedEntitySet.add(fullEntityName)
+        checkedEntityIndexSet.add(indexName)
         return true
     }
     @Override
     boolean checkAndAddTable(String entityName) {
         EntityDefinition ed
-        // just ignore EntityException on getEntityDefinition
-        try { ed = efi.getEntityDefinition(entityName) } catch (EntityException e) { return false }
-        // may happen if all entity names includes a DB view entity or other that doesn't really exist
+        try { ed = efi.getEntityDefinition(entityName) }
+        catch (EntityException e) { return false }
         if (ed == null) return false
-
-        // TODO
+        checkCreateDocumentIndex(ed)
+        return true
     }
     @Override
     int checkAndAddAllTables() {
@@ -111,22 +111,63 @@ class ElasticDatasourceFactory implements EntityDatasourceFactory {
 
     @Override
     EntityValue makeEntityValue(String entityName) {
-        EntityDefinition entityDefinition = efi.getEntityDefinition(entityName)
-        if (!entityDefinition) {
-            throw new EntityException("Entity not found for name [${entityName}]")
-        }
-
-        // TODO
-        return null
+        EntityDefinition ed = efi.getEntityDefinition(entityName)
+        if (ed == null) throw new EntityException("Entity not found for name ${entityName}")
+        return new ElasticEntityValue(ed, efi, this)
     }
 
     @Override
     EntityFind makeEntityFind(String entityName) { return new ElasticEntityFind(efi, entityName, this) }
 
     @Override
-    DataSource getDataSource() { return null }
+    DataSource getDataSource() {
+        throw new UnsupportedOperationException("DataSource not supported for ElasticFacade based Entity")
+    }
 
     void checkCreateDocumentIndex(EntityDefinition ed) {
-        // TODO
+        String indexName = getIndexName(ed)
+        if (checkedEntityIndexSet.contains(indexName)) return
+
+        ElasticFacade.ElasticClient elasticClient = efi.ecfi.elasticFacade.getClient(clusterName)
+        if (!elasticClient.indexExists(indexName))
+            elasticClient.createIndex(indexName, makeElasticEntityMapping(ed), null)
+
+        checkedEntityIndexSet.add(indexName)
+    }
+
+    String getIndexName(EntityDefinition ed) {
+        return indexPrefix + ed.getTableNameLowerCase()
+    }
+
+    static final Map<String, String> esEntityTypeMap = [id:'keyword', 'id-long':'keyword', date:'date', time:'keyword',
+            'date-time':'date', 'number-integer':'long', 'number-decimal':'double', 'number-float':'double',
+            'currency-amount':'double', 'currency-precise':'double', 'text-indicator':'keyword', 'text-short':'text',
+            'text-medium':'text', 'text-intermediate':'text', 'text-long':'text', 'text-very-long':'text', 'binary-very-long':'binary']
+    static final Set<String> esEntityIsKeywordSet = esEntityTypeMap.findAll({"keyword".equals(it.value)}).keySet()
+    static final Set<String> esEntityAddKeywordSet = new HashSet<>(['text-short', 'text-medium', 'text-intermediate'])
+
+    static Map makeElasticEntityMapping(EntityDefinition ed) {
+        Map<String, Object> rootProperties = [_entity:[type:'keyword']] as Map<String, Object>
+        Map<String, Object> mappingMap = [properties:rootProperties] as Map<String, Object>
+
+        FieldInfo[] allFieldInfo = ed.entityInfo.allFieldInfoArray
+        for (int i = 0; i < allFieldInfo.length; i++) {
+            FieldInfo fieldInfo = allFieldInfo[i]
+            rootProperties.put(fieldInfo.name, makeEntityFieldPropertyMap(fieldInfo))
+        }
+
+        return mappingMap
+    }
+    static Map makeEntityFieldPropertyMap(FieldInfo fieldInfo) {
+        String mappingType = esEntityTypeMap.get(fieldInfo.type) ?: 'text'
+        Map<String, Object> propertyMap = new LinkedHashMap<>()
+        propertyMap.put("type", mappingType)
+        if (esEntityAddKeywordSet.contains(fieldInfo.type) && "text".equals(mappingType))
+            propertyMap.put("fields", [keyword: [type: "keyword"]])
+        if ("date-time".equals(fieldInfo.type))
+            propertyMap.format = "date_time||epoch_millis||date_time_no_millis||yyyy-MM-dd HH:mm:ss.SSS||yyyy-MM-dd HH:mm:ss.S||yyyy-MM-dd"
+        else if ("date".equals(fieldInfo.type))
+            propertyMap.format = "date||strict_date_optional_time||epoch_millis"
+        return propertyMap
     }
 }
