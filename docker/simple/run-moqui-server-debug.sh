ba074ed8051d81facbf8e686cd48a29c3993c1d0
Michal Rovnanik on WS <michal@rovnanik.sk>
2023-02-05 21:10:24 +0100

    use DEV config when in debug mode

--- docker/simple/run-moqui-server-debug.sh
+++ docker/simple/run-moqui-server-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -cp . MoquiStart conf=conf/MoquiProductionConf.xml
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -cp . MoquiStart conf=conf/MoquiDevConf.xml
