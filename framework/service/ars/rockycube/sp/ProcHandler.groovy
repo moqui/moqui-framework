38002cf0273d3e3b6d9ea9ee688e98922a2fc938
Michal Rovnanik <michal@rovnanik.sk>
2025-02-06 01:08:14 +0100

    ProcHandler's service header

---
+++ framework/service/ars/rockycube/sp/ProcHandler.groovy
@@ -0,0 +1,13 @@
+import ars.rockycube.endpoint.ProcedureExecutor
+
+def launchWithParams()
+{
+    ProcedureExecutor pe = new ProcedureExecutor()
+    try {
+        return pe.executeProcedureWithParams(connectionName, procedureName, closureItemId)
+    } catch (Exception exc){
+        ec.logger.error("Procedure executor failed when executing: ${exc.message}")
+        ec.message.addPublic(exc.message, 'error')
+        throw exc
+    }
+}
