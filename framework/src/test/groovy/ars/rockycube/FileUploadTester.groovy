b673fe6e2601d17f3f58065dd20e4fc2319ab20d
Michal Rovnanik <michal@rovnanik.sk>
2025-08-26 15:15:38 +0200

    new test classes

--- framework/src/test/groovy/ars/rockycube/FileUploadTester.groovy
+++ framework/src/test/groovy/ars/rockycube/FileUploadTester.groovy
@@ -1,21 +1,28 @@
 package ars.rockycube
 
 import com.google.gson.Gson
+import org.apache.commons.fileupload.FileItem
 import org.moqui.Moqui
 import org.moqui.context.ExecutionContext
+import org.moqui.impl.context.ExecutionContextFactoryImpl
 import org.moqui.resource.ResourceReference
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import spock.lang.Shared
 import spock.lang.Specification
+import org.apache.commons.fileupload.disk.DiskFileItem
 
 class FileUploadTester extends Specification {
     protected final static Logger logger = LoggerFactory.getLogger(FileUploadTester.class)
     private Gson gson = new Gson()
     private String CONST_TEST_ENTITY = "ars.rockycube.test.TestFile"
+    private String CONST_TEST_ENTITY_2 = "ars.rockycube.test.ProjectFile"
 
     @Shared
     ExecutionContext ec
+
+    @Shared
+    ExecutionContextFactoryImpl ecfi
 
     def setupSpec() {
         System.setProperty("moqui.conf", "conf/MoquiDevConf.xml")
@@ -23,6 +30,9 @@ class FileUploadTester extends Specification {
         // init the framework, get the ec
         ec = Moqui.getExecutionContext()
         ec.user.loginUser('john.hardy', 'moqui')
+
+        // initialize ECFI
+        ecfi = (ExecutionContextFactoryImpl) Moqui.getExecutionContextFactory()
 
         // gson
         this.gson = new Gson()
@@ -33,6 +43,7 @@ class FileUploadTester extends Specification {
 
         //clean all
         ec.entity.find(CONST_TEST_ENTITY).deleteAll()
+        ec.entity.find(CONST_TEST_ENTITY_2).deleteAll()
     }
 
     def cleanupSpec() {
@@ -44,6 +55,14 @@ class FileUploadTester extends Specification {
             // stop it all
             ec.destroy()
         }
+    }
+
+    private FileItem createFileItem(ResourceReference rr) {
+        DiskFileItem fileItem = new DiskFileItem("file", "application/octet-stream", false, "directory.xlsx", 100, null)
+        OutputStream os = fileItem.getOutputStream()
+        os.write(rr.openStream().readAllBytes())
+        os.close()
+        return fileItem
     }
 
     def "test file upload - directly as bytes"() {
@@ -73,5 +92,47 @@ class FileUploadTester extends Specification {
         // SerialBlob size compared to the file uploaded
         def storedFile = loaded['file']
         assert storedFile.length() == rr.size
+    }
+
+    /**
+     * This test should simulate real-time usage, we need to use a standard service
+     * to process the request.
+     */
+    def "test file upload - using a service (obo IPS)"() {
+        when:
+
+        // load a file from resource
+        ResourceReference rr = ec.resource.getLocationReference("classpath://src/test/resources/FileUpload/directory.xlsx")
+        def uploadItem = createFileItem(rr)
+
+        // create a file
+        def result = this.ec.service.sync().name("sharepoint.FileServices.upload#File")
+                .disableAuthz()
+                .parameters([
+                        hostUrl: 'http://dev-01.srv',
+                        docGuid: "1111-22222-333333",
+                        entity: 'ars.rockycube.test.ProjectFile',
+                        file: uploadItem,
+                        path: 'whatever'
+                ])
+                .call()
+
+        then:
+
+        // assert size of the returned attachment as well
+        assert result
+
+        // load the contents
+        def content = this.ec.service.sync().name("sharepoint.FileServices.fetch#Bytes")
+                .disableAuthz()
+                .parameters([
+                        docGuid: "1111-22222-333333",
+                        entity: "ars.rockycube.test.ProjectFile"
+                ])
+                .call()
+
+        assert content
+        def storedContent = content['data']
+        assert storedContent.size() == rr.size
     }
 }
