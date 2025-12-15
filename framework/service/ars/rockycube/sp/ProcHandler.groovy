6d895b11c46a71ec03d09d90aa4ccc02193295d8
M.Rovnanik <michal@rovnanik.sk>
2025-12-15 21:17:09 +0100

    new functionality to call stored procedures and pass data in form of a list

--- framework/service/ars/rockycube/sp/ProcHandler.groovy
+++ framework/service/ars/rockycube/sp/ProcHandler.groovy
@@ -1,10 +1,29 @@
 import ars.rockycube.endpoint.ProcedureExecutor
 
-def launchWithParams()
-{
+/**
+ * Launch a stored procedure and pass data of closureItemId into the procedure,
+ * the procedure should use this parameter to load related data
+ *  @return data
+ */
+def launchWithParams() {
     ProcedureExecutor pe = new ProcedureExecutor()
     try {
         return pe.executeProcedureWithParams(connectionName, procedureName, companyId, closureItemId)
+    } catch (Exception exc){
+        ec.logger.error("Procedure executor failed when executing: ${exc.message}")
+        ec.message.addPublic(exc.message, 'error')
+        throw exc
+    }
+}
+
+/**
+ * Launch stored procedure and use direct data
+ * @return data
+ */
+def launchWithList() {
+    ProcedureExecutor pe = new ProcedureExecutor()
+    try {
+        return pe.executeProcedureWithList(connectionName, procedureName, companyId, data)
     } catch (Exception exc){
         ec.logger.error("Procedure executor failed when executing: ${exc.message}")
         ec.message.addPublic(exc.message, 'error')
