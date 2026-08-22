023572fc70173b8382f0cf9809f537c5983d8f58
Deepak Dixit <deepak.dixit@hotwax.co>
2026-08-22 13:21:38 +0530

    Used Origin instead custom header to identity the origin

--- framework/src/main/groovy/org/moqui/impl/context/ExecutionContextImpl.java
+++ framework/src/main/groovy/org/moqui/impl/context/ExecutionContextImpl.java
@@ -192,8 +192,8 @@ public class ExecutionContextImpl implements ExecutionContext {
         if (userId != null && !userId.isEmpty()) MDC.put("moqui_userId", userId);
         String visitorId = userFacade.getVisitorId();
         if (visitorId != null && !visitorId.isEmpty()) MDC.put("moqui_visitorId", visitorId);
-        String appName = request.getHeader("X-Application-Name");
-        if (appName != null && !appName.isEmpty()) MDC.put("maarg_appName", appName);
+        String appName = request.getHeader("Origin");
+        if (appName != null && !appName.isEmpty()) MDC.put("maarg_appName", "[app:" + appName + "]");
 
         if (loggerDirect.isTraceEnabled()) loggerDirect.trace("ExecutionContextImpl WebFacade initialized");
     }
